5ec097385f382b7742ecb39111d47258ed282d26
João Sereno <joaohsereno@tecnico.ulisboa.pt>
2024-03-08 18:13:36 +0000

    chore: add offline dependencies

--- HDSLedger/install_deps.sh
+++ HDSLedger/install_deps.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 mvn install:install-file -Dfile=dependencies/gson-2.10.1.jar -DgroupId="com.google.code.gson" -DartifactId="gson" -Dversion="2.10.1" -Dpackaging="jar"
+mvn install:install-file -Dfile=dependencies/commons-lang3-3.12.0.jar -DgroupId="org.apache.commons" -DartifactId="commons-lang3" -Dversion="3.12.0" -Dpackaging="jar"
