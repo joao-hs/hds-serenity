b95cad72e365721214f282cd239b3022a66ccb4d
João Sereno <joaohsereno@tecnico.ulisboa.pt>
2024-03-31 23:44:56 +0100

    fix: bugs

--- HDSLedger/Service/src/main/java/pt/ulisboa/tecnico/hdsledger/service/models/ProgressIndicator.java
+++ HDSLedger/Service/src/main/java/pt/ulisboa/tecnico/hdsledger/service/models/ProgressIndicator.java
@@ -3,7 +3,7 @@ package pt.ulisboa.tecnico.hdsledger.service.models;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ProgressIndicator {
-    private AtomicInteger timeout = new AtomicInteger(1000); // ms
+    private AtomicInteger timeout = new AtomicInteger(5000); // ms
     private Boolean isMakingProgress = Boolean.FALSE;
     private final Object lock = new Object();
 
